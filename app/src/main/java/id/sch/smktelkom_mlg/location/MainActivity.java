e5d211e03e2ca3afddcb1791c8f75d56deeca9a8
Ladinar Nanda Aprilia <ladinarnanda@gmail.com>
2017-10-09 16:04:50 +0700

    initial commit

---
+++ app/src/main/java/id/sch/smktelkom_mlg/location/MainActivity.java
@@ -0,0 +1,32 @@
+package id.sch.smktelkom_mlg.location;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+
+public class MainActivity extends AppCompatActivity implements GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener {
+
+    
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public void onConnected(Bundle bundle) {
+
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+
+    }
+
+    @Override
+    public void onConnectionFailed(ConnectionResult connectionResult) {
+
+    }
+}
