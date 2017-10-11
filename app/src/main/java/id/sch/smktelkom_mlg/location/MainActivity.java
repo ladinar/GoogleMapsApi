5d5de60252427058d0d47d38de2a0b0ec366ef45
Ladinar Nanda Aprilia <ladinarnanda@gmail.com>
2017-10-11 15:40:53 +0700

    adding API KEY

--- app/src/main/java/id/sch/smktelkom_mlg/location/MainActivity.java
+++ app/src/main/java/id/sch/smktelkom_mlg/location/MainActivity.java
@@ -2,17 +2,32 @@ package id.sch.smktelkom_mlg.location;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
 
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
 
 public class MainActivity extends AppCompatActivity implements GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener {
 
-    
+    private final String LOG_TAG = "LaurenceTestApp";
+    private TextView txtOutput;
+    private GoogleApiClient mGoogleApiClient;
+    private LocationRequest mLocationRequest;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .addApi(LocationServices.API)
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .build();
+
+        txtOutput = (TextView) findViewById(R.id.txtOutput);
     }
 
     @Override
